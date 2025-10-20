473aa1ac6c9fdbb8f1df5efb531401ac470e757f
wonCha25 <chawj1234@gmail.com>
2025-10-20 23:11:14 +0900

    SwiftData 연결 관련 설정
    - SwiftData를 연결하기 위해서 gachaApp.swift 파일에 ModelContainer 설정을 추가했습니다.
    - 기존의 데이터 추가 View와 테이터를 시각화하는 SwiftChart View를 탭으로 구분했습니다.

--- gacha/gacha/gachaApp.swift
+++ gacha/gacha/gachaApp.swift
@@ -10,12 +10,45 @@ import SwiftData
 
 @main
 struct gachaApp: App {
+    
+    /// swiftData에서 데이터를 저장, 읽기 위해서 ModelContainer 필요
+    /// ModelContainer는 앱이 시작될 때 한 번만 설정되어야 해서 init( ) 에서 초기화한다.
+    let modelContainer: ModelContainer
+    
+    init() {
+        do {
+            /// swiftData가 저장할 모델 목록 정의
+            let schema = Schema([MesuredRecord.self])
+            /// 실제 저장 위치와 설정 정의
+            /// schema: 모델 목록
+            /// isStoredInMemoryOnly: false(앱 종료 후에도 유지), true(앱 종료 시 데이터 삭제)
+            let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+            
+            /// 최종 컨테이너 생성
+            /// for: SwiftData가 관리할 모델
+            /// configurations: 여러 데이터 저장소 설정을 배열로 받는다.
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            print("✅ ModelContainer 초기화 성공")
+        } catch {
+            /// 에러 처리
+            fatalError("ModelContainer 초기화 실패: \(error)")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .modelContainer(for: [
-                    MesuredRecord.self,
-                ])
+            TabView {
+                ContentView()
+                    .tabItem {
+                        Label("데이터 관리", systemImage: "list.bullet")
+                    }
+                
+                WeightChartView()
+                    .tabItem {
+                        Label("차트", systemImage: "chart.line.uptrend.xyaxis")
+                    }
+            }
         }
+        .modelContainer(modelContainer)
     }
 }
