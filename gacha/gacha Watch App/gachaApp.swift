1b9cbed71895b476d6944478dce5bd436f9a0872
ohdodin <seojin2414@naver.com>
2025-10-20 12:05:20 +0900

    [chore] 프로젝트 세팅
    - .gitignore 파일 세팅
    - 기본 세팅

---
+++ gacha/gacha Watch App/gachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gachaApp.swift
+//  gacha Watch App
+//
+//  Created by Oh Seojin on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct gacha_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
