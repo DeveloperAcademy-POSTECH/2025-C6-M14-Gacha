bba8c4e861be31dd931b5e7417c04a387249ff4b
ohdodin <seojin2414@naver.com>
2025-11-10 05:07:46 +0900

    Fix: #83 통증 설명 가운데 정렬

--- gacha/gacha/Views/Measure/PainLevel.swift
+++ gacha/gacha/Views/Measure/PainLevel.swift
@@ -73,12 +73,11 @@ struct PainLevel: View {
                 Spacer()
                 
                 // MARK: - 이모지
-                VStack(spacing: 16) {
+                VStack (spacing: 16) {
                     Image(painImageName(for: Int(value)))
                         .resizable()
                         .scaledToFit()
                         .frame(width: 136, height: 136)
-                    //원준띠니얌~
                     VStack (spacing: 8) {
                         Text(Strings.PainCategory.category(for: Int(value)))
                             .font(.displayTitle1Bold)
@@ -87,6 +86,7 @@ struct PainLevel: View {
                             .font(.displayBodyRegular)
                             .foregroundStyle(Color("Gray700"))
                             .frame(height: 44)
+                            .multilineTextAlignment(.center)
                     }
                 }
                 .padding(.bottom, 40)
@@ -359,11 +359,6 @@ struct ArcSlider: View {
             }
         }
     }
-
-//    private func levelDescription(for value: Double) -> String {
-//        //원준띠니얌~
-//        //return Strings.Pain.level(Int(value))
-//    }
 }
 
 // MARK: - 세그먼트 원호 Shape
@@ -450,4 +445,3 @@ struct ArcShape: Shape {
     return PainLevel()
         .environmentObject(viewModel)
 }
-
